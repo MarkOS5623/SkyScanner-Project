ce6b49be9f705bb163b795c6c61e5b921bc44a12
MarkOS5623 <markos5623@gmail.com>
2023-01-05 19:32:49 +0200

    Fixxed some visuals

---
+++ SkyScanner/Models/CreditCard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SkyScanner.Models
+{
+    public class CreditCard
+    {
+        public string User_ID { get; set; }
+        [ForeignKey("User_ID")]
+        public virtual User User { get; set; }
+        [Key]
+        public string CardNumber { get; set; }
+        public int ExpMonth { get; set; }
+        public int ExpYear { get; set; }
+        public int CVV { get; set; }
+    }
+}
